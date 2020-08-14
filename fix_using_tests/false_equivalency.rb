644ad1efbdeae3e1f3d24da7d43c0d3b2c7f965f
Justin Page <justin.page89@gmail.com>
2020-08-14 16:44:04 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -17,7 +17,7 @@ def selection(num)
   
      "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
-
+  binding.pry
      "HAM HAM HAM IN MY TUMMY"
   end
 end
@@ -25,6 +25,4 @@ end
 def runner
   prompt_user
   selection(get_user_input)
-  binding.pry
-  puts 'hello'
 end
